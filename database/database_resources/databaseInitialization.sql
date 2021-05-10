24a8fe0740f0eee367d60ec383ddb1a20bb1b246
Volitair <vasilsinishin@gmail.com>
2021-05-11 02:53:54 +0300

    added database initialization, initialization data and queries

---
+++ database/database_resources/databaseInitialization.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS Public.Task_Status (
+  status_id serial NOT NULL,
+  name varchar NOT NULL,
+  PRIMARY KEY (status_id)
+);
+
+CREATE TABLE IF NOT EXISTS Public.Task_Complexity (
+  complexity_id serial NOT NULL,
+  name varchar NOT NULL,
+  PRIMARY KEY (complexity_id)
+);
+
+CREATE TABLE IF NOT EXISTS Public.Users (
+  user_id serial NOT NULL,
+  name varchar NOT NULL,
+  surname varchar NOT NULL,
+  password varchar NOT NULL,
+  max_working_time int NOT NULL,
+  PRIMARY KEY (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS Public.Tasks (
+  task_id serial NOT NULL,
+  name varchar NOT NULL,
+  description varchar NOT NULL,
+  complexity int NOT NULL,
+  execution_time int NOT NULL,
+  status int NOT NULL,
+  PRIMARY KEY (task_id),
+  CONSTRAINT Tasks_complexity_foreign FOREIGN KEY (complexity) REFERENCES Public.Task_Complexity (complexity_id),
+  CONSTRAINT Tasks_status_foreign FOREIGN KEY (status) REFERENCES Public.Task_Status (status_id)
+);
+
+CREATE TABLE IF NOT EXISTS Public.User_Tasks (
+  user_id int NOT NULL,
+  task_id int NOT NULL,
+  time_spent int NOT NULL,
+  CONSTRAINT UserTasks_user_id_foreign FOREIGN KEY (user_id) REFERENCES Public.Users (user_id),
+  CONSTRAINT UserTasks_task_id_foreign FOREIGN KEY (task_id) REFERENCES Public.Tasks (task_id)
+);
---
+++ database/database_resources/initialData.sql
@@ -0,0 +1,33 @@
+INSERT INTO Task_Complexity VALUES
+  (default, 'Низький'),
+  (default, 'Середній'),
+  (default, 'Високий');
+
+INSERT INTO Task_Status VALUES
+  (default, 'В процесі виконання'),
+  (default, 'Завершено');
+
+INSERT INTO Users VALUES
+  (default, 'Тарас', 'Барабаш', 'jenf', 600),
+  (default, 'Євген', 'Родичев', 'ooiw', 500),
+  (default, 'Назарій', 'Киричук', 'owed', 620),
+  (default, 'Ігор', 'Вихорь', 'mswv', 530),
+  (default, 'Варфоломій', 'Лимоненко', 'mswb', 550);
+
+INSERT INTO Tasks VALUES
+  (default, 'Лоботомія', 'лоботомія', 2, 1200, 1),
+  (default, 'Галоперидол', 'галоперидол', 3, 1400, 1),
+  (default, 'Шокова терапія', 'шокова терапія', 2, 1100, 1),
+  (default, 'Групова терапія', 'Групова терапія', 1, 1450, 1);
+
+INSERT INTO User_Tasks VALUES
+  (1, 2, 120),
+  (1, 3, 150),
+  (2, 4, 60),
+  (2, 1, 190),
+  (2, 4, 145),
+  (3, 3, 185),
+  (4, 2, 125),
+  (4, 4, 160),
+  (5, 1, 140);
+
