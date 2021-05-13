41d0ab37a36f2669df2ae2682cc2f9b01b7a03ad
LovelyDrug <suhanovamasha21@gmail.com>
2021-05-13 21:55:08 +0300

    Added htmls first version

---
+++ FrontEnd/script/main.js
@@ -0,0 +1,17 @@
+var modal1 = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
+
+var modal2 = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
