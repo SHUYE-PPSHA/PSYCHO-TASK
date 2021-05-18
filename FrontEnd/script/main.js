18afd5f7de9e852ce71f90480845817abd4b03f9
LovelyDrug <suhanovamasha21@gmail.com>
2021-05-18 03:45:32 +0300

    Added FrontEnd tests

--- FrontEnd/script/main.js
+++ FrontEnd/script/main.js
@@ -1,26 +1,71 @@
-var modal1 = document.getElementById('id01');
+'use strict';
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal1) {
-        modal1.style.display = "none";
+const modal1 = document.getElementById('id01');
+const modal2 = document.getElementById('id02');
+const modal3 = document.getElementById('id03');
+
+let accounts = [];
+
+class account {
+  
+  async signUp(mail, usr, psswrd, repeatpsswrd) {
+    if ( typeof mail, usr, psswrd, repeatpsswrd !== 'string') 
+    {
+      throw new Error('Data has a wrong type');
     }
+
+    if (accounts.includes(mail || usr || psswrd) === true) 
+    {
+      throw new Error('This account already exist');
+    }
+
+    if (psswrd !== repeatpsswrd) 
+    {
+      throw new Error('Repeated password does not match the password')
+    }
+
+    let acc = {email: mail, username: usr, password: psswrd, repeatedPassword: repeatpsswrd};
+    accounts.push[acc];
+    return accounts;
+  }
+
+  async login (mail, usr, psswrd) {
+    if (mail, usr, psswrd !== 'string') 
+    {
+      throw new Error('String is expected');
+    }
+
+    if (accounts.includes(mail || usr || psswrd) !== true) 
+    {
+      throw new Error('The account does not exist');
+    }
+
+  }
 }
 
-var modal2 = document.getElementById('id02');
+class task {
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal2) {
-        modal2.style.display = "none";
+  async addTask(name, descrip, priority, deadline) 
+  {
+    if (name, descrip, deadline !== 'string' || priority !== 'number') 
+    {
+      throw new Error('Different types of input are expected');
     }
-}
 
-var modal3 = document.getElementById('id03');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal3) {
-        modal2.style.display = "none";
+    if (!name) 
+    {
+      throw new Error('Task is expected to have a name');
     }
+    if (1 > priority || priority > 5) 
+    {
+      throw new Error('Priority number is out of range');
+    }
+
+    if (Number(deadline) < Date.now) 
+    {
+      throw new Error('Deadline can not be earlier than today');
+    }
+  }
+
+  async changeTask() {}
 }
---
+++ test/front.test.js
@@ -0,0 +1,126 @@
+'use strict';
+
+const assert = require('assert').strict;
+
+describe('frontEnd tests', () => {
+    it('account.signUp_wrongTypeOfInput', async () => {
+      const acc = {
+        email: 'suh@gmail.com',
+        username: 'sqe',
+        password: 1234,
+        repeatedPassword: 1224,
+      };
+      try {
+        await acc.signUp(acc.email, acc.username, acc.password, acc.repeatedPassword);
+      } catch (e) {
+        assert.equal(e.message, 'Data has a wrong type. Expected string');
+      }
+    });
+
+    it('account.signUp_AlreadyExist', async () => {
+        const acc = {
+            email: 'suh@gmail.com',
+            username: 'sqe',
+            password: '1234',
+            repeatedPassword: '1234',
+        };
+        try {
+            await acc.signUp(acc.email, acc.username, acc.password, acc.repeatedPassword);
+        } catch (e) {
+            assert.equal(e.message, 'This account already exist');
+        }
+    });
+
+    it('account.signUp_ReapetpsswrdWrongInput', async () => {
+        const acc = {
+            email: 'suh@gmail.com',
+            username: 'sqe',
+            password: '123444242',
+            repeatedPassword: '142532521',
+        };
+        try {
+            await acc.signUp(acc.email, acc.username, acc.password, acc.repeatedPassword);
+        } catch (e) {
+            assert.equal(e.message, 'Repeated password does not match the password');
+        }
+    });
+
+    
+    it('account.login_notStringInput', async () => {
+        const acc = {
+            email: 'suh@gmail.com',
+            username: sqe,
+            password: 12344,
+        };
+        try {
+            await acc.login(acc.email, acc.username, acc.password);
+        } catch (e) {
+            assert.equal(e.message, 'String is expected');
+        }
+    });
+    
+    it('account.login_NotExist', async () => {
+        const acc = {
+            email: 'suh@gmail.com',
+            username: 'sqe',
+            password: '123444242',
+        };
+        try {
+            await acc.login(acc.email, acc.username, acc.password);
+        } catch (e) {
+            assert.equal(e.message, 'The account does not exist');
+        }
+    });
+    
+    it('task.addTask_WrongTypeInput', async () => {
+        const task = {
+            name: tests,
+            priority: '4',
+            deadline: '11.06.2021',
+        };
+        try {
+            await task.addTask(task.name, task.priority, task.deadline);
+        } catch (e) {
+            assert.equal(e.message, 'Different types of input are expected');
+        }
+    });
+    
+    it('task.addTask_NoName', async () => {
+        const task = {
+            name: '',
+            priority: 4,
+            deadline: '11.06.2021',
+        };
+        try {
+            await task.addTask(task.name, task.priority, task.deadline);
+        } catch (e) {
+            assert.equal(e.message, 'Task is expected to have a name');
+        }
+    });
+    
+    it('task.addTask_OutOfRange', async () => {
+        const task = {
+            name: 'tests',
+            priority: 424,
+            deadline: '11.06.2021',
+        };
+        try {
+            await task.addTask(task.name, task.priority, task.deadline);
+        } catch (e) {
+            assert.equal(e.message, 'Priority number is out of range');
+        }
+    });
+
+    it('task.addTask_UnexpectedDeadline', async () => {
+        const task = {
+            name: 'tests',
+            priority: 4,
+            deadline: '11.01.2019',
+        };
+        try {
+            await task.addTask(task.name, task.priority, task.deadline);
+        } catch (e) {
+            assert.equal(e.message, 'Deadline can not be earlier than today');
+        }
+    });
+})
