a81463b3f696a3d4142e4357f40cacc052a1c467
LovelyDrug <suhanovamasha21@gmail.com>
2021-05-24 21:39:06 +0300

    fix tests

--- test/front.test.js
+++ test/front.test.js
@@ -1,49 +1,38 @@
 'use strict';
 
 const assert = require('assert').strict;
+const { App } = require('../static/js/main.js');
+const app1 = new App({});
 
 describe('frontEnd tests', () => {
     it('account.signUp_wrongTypeOfInput', async () => {
       const acc = {
         email: 'suh@gmail.com',
-        username: 'sqe',
+        usrname: 'sqe',
+        usrSurName: 'fsf',
         password: 1234,
         repeatedPassword: 1234,
       };
       try {
-        await acc.signUp(acc.email, acc.username, acc.password, acc.repeatedPassword);
-      } catch (e) {
-        assert.equal(e.message, 'Data has a wrong type. Expected string');
+        await app1.account.signUp(acc.email, acc.usrname, acc.usrSurName, acc.password, acc.repeatedPassword);
+      } catch (error) {
+        assert.equal(error.message, 'Data has a wrong type');
       }
     });
 
     it('account.signUp_wrongRepeatedPassword', async () => {
       const acc = {
         email: 'suh@gmail.com',
-        username: 'sqe',
+        usrname: 'sqe',
+        usrSurName: 'fsf',
         password: '1234',
         repeatedPassword: '1224',
       };
       try {
-        await acc.signUp(acc.email, acc.username, acc.password, acc.repeatedPassword);
-      } catch (e) {
-        assert.equal(e.message, 'Data has a wrong repeated password');
+        await app1.account.signUp(acc.email, acc.usrname, acc.usrSurName, acc.password, acc.repeatedPassword);
+      } catch (error) {
+        assert.equal(error.message, 'Data has a wrong repeated password');
       }
-    });
-
-
-    it('account.signUp_AlreadyExist', async () => {
-        const acc = {
-            email: 'suh@gmail.com',
-            username: 'sqe',
-            password: '1234',
-            repeatedPassword: '1234',
-        };
-        try {
-            await acc.signUp(acc.email, acc.username, acc.password, acc.repeatedPassword);
-        } catch (e) {
-            assert.equal(e.message, 'This account already exist');
-        }
     });
 
     it('account.signUp_ReapetpsswrdWrongInput', async () => {
@@ -54,7 +43,7 @@ describe('frontEnd tests', () => {
             repeatedPassword: '142532521',
         };
         try {
-            await acc.signUp(acc.email, acc.username, acc.password, acc.repeatedPassword);
+            await app1.account.signUp(acc.email, acc.username, acc.password, acc.repeatedPassword);
         } catch (e) {
             assert.equal(e.message, 'Repeated password does not match the password');
         }
@@ -68,7 +57,7 @@ describe('frontEnd tests', () => {
             repeatedPassword: '1234',
         };
         try {
-            await acc.signUp(acc.email, acc.username, acc.password, acc.repeatedPassword);
+            await app1.account.signUp(acc.email, acc.username, acc.password, acc.repeatedPassword);
         } catch (e) {
             assert.equal(e.message, 'Invalid Email');
         }
@@ -82,7 +71,7 @@ describe('frontEnd tests', () => {
             repeatedPassword: '1234',
         };
         try {
-            await acc.signUp(acc.email, acc.username, acc.password, acc.repeatedPassword);
+            await app1.account.signUp(acc.email, acc.username, acc.password, acc.repeatedPassword);
         } catch (e) {
             assert.equal(e.message, 'Long email');
         }
@@ -96,7 +85,7 @@ describe('frontEnd tests', () => {
             repeatedPassword: '1234',
         };
         try {
-            await acc.signUp(acc.email, acc.username, acc.password, acc.repeatedPassword);
+            await app1.account.signUp(acc.email, acc.username, acc.password, acc.repeatedPassword);
         } catch (e) {
             assert.equal(e.message, 'Long username');
         }
@@ -109,7 +98,7 @@ describe('frontEnd tests', () => {
             password: 12344,
         };
         try {
-            await acc.login(acc.email, acc.username, acc.password);
+            await app1.account.login(acc.email, acc.username, acc.password);
         } catch (e) {
             assert.equal(e.message, 'String is expected');
         }
@@ -122,85 +111,85 @@ describe('frontEnd tests', () => {
             password: '123444242',
         };
         try {
-            await acc.login(acc.email, acc.username, acc.password);
+            await app1.account.login(acc.email, acc.username, acc.password);
         } catch (e) {
             assert.equal(e.message, 'The account does not exist');
         }
     });
     
     it('task.addTask_WrongTypeInput', async () => {
-        const task = {
+        const tassk = {
             name: tests,
             priority: '4',
             deadline: '11.06.2021',
         };
         try {
-            await task.addTask(task.name, task.priority, task.deadline);
+            await app1.task.addTask(tassk.name, tassk.priority, tassk.deadline);
         } catch (e) {
             assert.equal(e.message, 'Different types of input are expected');
         }
     });
     
     it('task.addTask_NoName', async () => {
-        const task = {
+        const tassk = {
             name: '',
             priority: 4,
             deadline: '11.06.2021',
         };
         try {
-            await task.addTask(task.name, task.priority, task.deadline);
+            await app1.task.addTask(tassk.name, tassk.priority, tassk.deadline);
         } catch (e) {
             assert.equal(e.message, 'Task is expected to have a name');
         }
     });
 	
     it('task.addTask_LongName', async () => {
-        const task = {
+        const tassk = {
             name: 'qwertyuiopasdfghjkl;zxcvbnm,mnbvcxzasdfghjkl;;poiuytrewqasdfghjkjhgfddfthgytrfghjutrdcvbk',
             priority: 4,
             deadline: '11.06.2021',
         };
         try {
-            await task.addTask(task.name, task.priority, task.deadline);
+            await app1.task.addTask(tassk.name, tassk.priority, tassk.deadline);
         } catch (e) {
             assert.equal(e.message, 'Long task name');
         }
     });
     
     it('task.addTask_NoName', async () => {
-        const task = {
+        const tassk = {
             name: null,
             priority: 4,
             deadline: '11.06.2021',
         };
         try {
-            await task.addTask(task.name, task.priority, task.deadline);
+            await app1.task.addTask(tassk.name, tassk.priority, tassk.deadline);
         } catch (e) {
             assert.equal(e.message, 'Task is expected to have a name');
         }
     });
     
     it('task.addTask_OutOfRange', async () => {
-        const task = {
+        const tassk = {
             name: 'tests',
             priority: 424,
             deadline: '11.06.2021',
         };
         try {
-            await task.addTask(task.name, task.priority, task.deadline);
+            await app1.task.addTask(tassk.name, tassk.priority, tassk.deadline);
         } catch (e) {
             assert.equal(e.message, 'Priority number is out of range');
         }
     });
 
     it('task.addTask_UnexpectedDeadline', async () => {
-        const task = {
+        const tassk = {
             name: 'tests',
             priority: 4,
             deadline: '11.01.2019',
         };
         try {
-            await task.addTask(task.name, task.priority, task.deadline);
+            await app1.task.addTask(tassk.name, tassk.priority, tassk.deadline);
         } catch (e) {
             assert.equal(e.message, 'Deadline can not be earlier than today');
         }
